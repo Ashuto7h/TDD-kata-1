73b7f671207670416bab90374e45dd77913a6e36
Ashuto7h <ashusahu198@gmail.com>
2021-04-20 17:11:11 +0530

    added support for different delimiters

--- StringCalculator.java
+++ StringCalculator.java
@@ -1,7 +1,19 @@
 class StringCalculator {
     public int Add(String numbers) {
         int sum = 0;
-        String nums[] = numbers.split(",|\n");
+        char delimiters[];
+        String nums[];
+        if (numbers.substring(0, 2).equals("//")) {
+            String inputs[] = numbers.split("\n", 2);
+            delimiters = inputs[0].substring(2).toCharArray();
+            String splitter = ",|\n";
+            for (char i : delimiters) {
+                splitter += "|" + i;
+            }
+            nums = inputs[1].split(splitter);
+        } else {
+            nums = numbers.split(",|\n");
+        }
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] == "")
                 continue;
@@ -12,7 +24,7 @@ class StringCalculator {
 
     public static void main(String[] args) {
         StringCalculator sc = new StringCalculator();
-        int ans = sc.Add("1,2\n3,4");
+        int ans = sc.Add("//;-\n3-4;5,4\n5");
         System.out.println(ans);
     }
 }

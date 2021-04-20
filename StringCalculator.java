9667092f2d596d985ce6705ab956b9ddbfb52001
Ashuto7h <ashusahu198@gmail.com>
2021-04-20 16:23:53 +0530

    allowed Add method to handle any amount of numbers

--- StringCalculator.java
+++ StringCalculator.java
@@ -2,7 +2,7 @@ class StringCalculator {
     public int Add(String numbers) {
         int sum = 0;
         String nums[] = numbers.split(",");
-        for (int i = 0; i < Integer.min(nums.length, 3); i++) {
+        for (int i = 0; i < nums.length; i++) {
             if (nums[i] == "")
                 continue;
             sum += Integer.parseInt(nums[i]);
@@ -12,7 +12,7 @@ class StringCalculator {
 
     public static void main(String[] args) {
         StringCalculator sc = new StringCalculator();
-        int ans = sc.Add("1,2,3");
+        int ans = sc.Add("");
         System.out.println(ans);
     }
 }

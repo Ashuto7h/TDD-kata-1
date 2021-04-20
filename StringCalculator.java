ec4606a51d98b358d54b7effae41b95ecb56371e
Ashuto7h <ashusahu198@gmail.com>
2021-04-20 16:21:59 +0530

    corrected a minor error at line 5

--- StringCalculator.java
+++ StringCalculator.java
@@ -2,7 +2,7 @@ class StringCalculator {
     public int Add(String numbers) {
         int sum = 0;
         String nums[] = numbers.split(",");
-        for (int i = 0; i < Integer.min(nums.length, 2); i++) {
+        for (int i = 0; i < Integer.min(nums.length, 3); i++) {
             if (nums[i] == "")
                 continue;
             sum += Integer.parseInt(nums[i]);
@@ -12,7 +12,7 @@ class StringCalculator {
 
     public static void main(String[] args) {
         StringCalculator sc = new StringCalculator();
-        int ans = sc.Add("");
+        int ans = sc.Add("1,2,3");
         System.out.println(ans);
     }
 }
